81b3367c0deef8c21f1dd46d39d359c2e07dcbc7
osama-abdullah <50793839+osama-abdullah@users.noreply.github.com>
2022-09-10 09:39:19 +0300

    add more pages and also get issues attributes and append it to the results

--- DataScraping.py
+++ DataScraping.py
@@ -25,10 +25,39 @@ class GithubAPI:
         self.raw = []
 
         r = requests.get(url, params=self.issues_payload, headers=self.auth).json()
-        print(pages)
         
-        
+        for i in range(pages):
+            self.raw += r
 
-        return self.results.append(r)
+            if len(r) == 100:
+                self.issues_payload["page"] += 1
+                r = requests.get(url, params=self.issues_payload, headers=self.auth).json()
+            else:
+                break
+        for e in self.raw:
+            print("Checking issue " + str(e["number"]))
+
+            issue = collections.OrderedDict()
+            issue["id"] = e["id"]
+            issue["number"] = e["number"]
+            issue["repo_url"] = e["repository_url"]
+            issue["issue_url"] = e["url"]
+            issue["events_url"] = e["events_url"]
+            issue["state"] = e["state"]
+            issue["html_url"] = e["html_url"]
+            issue["title"] = e["title"]
+            issue["description"] = e["body"]
+            issue["comments"] = e["comments"]
+            issue["created_at"] = e["created_at"]
+            issue["updated_at"] = e["updated_at"]
+            issue["closed_at"] = e["closed_at"]
+            if not e["milestone"]:
+                 issue["milestone"] = "null"
+            else:
+                 issue["milestone"] = e["milestone"]["title"]
+
+            
+
+            self.results.append(issue)
 
    
