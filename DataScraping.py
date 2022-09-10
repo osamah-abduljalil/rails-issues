4e0651b1e39a64cb42f8873f946dd725560d02d7
osama-abdullah <50793839+osama-abdullah@users.noreply.github.com>
2022-09-10 09:34:45 +0300

    add getIssues function and test the request

--- DataScraping.py
+++ DataScraping.py
@@ -1,3 +1,4 @@
+from unittest import result
 import requests
 import collections
 from openpyxl import Workbook
@@ -12,5 +13,22 @@ class GithubAPI:
         "state": "all",
     }
     auth = {"Authorization": "token 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"}
+
+    def getIssues(self, url,pages):
+
+        self.issues_payload = {
+        "per_page": 100,
+        "page": 1,
+        "state": "all",
+        }
+
+        self.raw = []
+
+        r = requests.get(url, params=self.issues_payload, headers=self.auth).json()
+        print(pages)
+        
+        
+
+        return self.results.append(r)
 
    
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from DataScraping import GithubAPI
 api=GithubAPI()
+issues = api.getIssues("https://api.github.com/repos/rails/rails/issues",5)
+print(issues)
